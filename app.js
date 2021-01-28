16e1d894984b925ad7bc38efcee1e4a5cf4f7f77
petran <petranb2@gmail.com>
2021-01-29 00:52:55 +0200

    Add compression to express app

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-
+var compression = require('compression')
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
@@ -12,6 +12,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression())
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("*", function (req, res, next) {
